78ecd0ac3d69f8ea9c5ee70ae131fec0c2e7521b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-03-03 13:44:03 +0100

    refactor StartGame

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour
 {
 
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
     int guess;
 
     // Use this for initialization
@@ -15,10 +20,10 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        max = max + 1;
+     
         guess = (max + min) / 2;
-
-
+        guessText.text = guess.ToString();
+        max = max + 1;
 
     }
 
@@ -37,5 +42,6 @@ public class NumberWizard : MonoBehaviour
     void NextGuess()
     {
         guess = (max + min) / 2;
+        guessText.text = guess.ToString();
     }
 }
