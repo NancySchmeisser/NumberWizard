464054085fadf9c73da5e0735c0dc7cdd426ce10
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-03-03 12:46:25 +0100

    refactor NumberWizard

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
 
-    int max;
-    int min;
+    [SerializeField] int max;
+    [SerializeField] int min;
     int guess;
 
     // Use this for initialization
@@ -17,41 +15,27 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        max = 1000;
-        min = 1;
-        guess = 500;
-
-        Debug.Log("G'day mate, welcome to numba wiz-ud...");
-        Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than: " + guess);
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         max = max + 1;
+        guess = (max + min) / 2;
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPressHigher()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("I am a genius!");
-            StartGame();
-        }
+        min = guess;
+        NextGuess();
     }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
     void NextGuess()
     {
         guess = (max + min) / 2;
-        Debug.Log("Is it higher or lower than..." + guess);
     }
 }
